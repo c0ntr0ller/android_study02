f594d8b963a2e48337bea2c48029e56808693fe6
bek <bek.andrey@gmail.com>
2021-02-08 16:03:08 +0700

    add action on buttons add second activity

--- app/src/main/java/ru/progmatik/study02/MainActivity.kt
+++ app/src/main/java/ru/progmatik/study02/MainActivity.kt
@@ -2,10 +2,29 @@ package ru.progmatik.study02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun toastMe(view: View){
+        val myToast = Toast.makeText(this, "Hello, Toast!", Toast.LENGTH_SHORT)
+
+        myToast.show()
+    }
+
+    fun countMe(view: View){
+        val currCount = textViewCount.text.toString()
+
+        var newCount: Int = Integer.parseInt(currCount)
+
+        newCount++
+
+        textViewCount.text = newCount.toString()
     }
 }
---
+++ app/src/main/java/ru/progmatik/study02/SecondActivity.kt
@@ -0,0 +1,11 @@
+package ru.progmatik.study02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
